e43549f6650ade6f2a1e5515ab5c260d67fceac9
Victor Litvynchuk <vendettich13@icloud.com>
2023-04-01 00:04:22 +0300

    пушу для стягнення пакетів

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -2,22 +2,13 @@ import styled from 'styled-components';
 import theme from '../style/generalStyle';
 
 export const AppBox = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 40;
-
-  padding-left: 16px;
-  padding-right: 16px;
+  padding: 20px 16px;
 
   @media ${theme.device.tablet} {
-    padding-left: 32px;
-    padding-right: 32px;
+    padding: 20px 32px;
   }
 
   @media ${theme.device.desktop} {
-    padding-left: 100px;
-    padding-right: 100px;
+    padding: 14px 100px;
   }
 `;
