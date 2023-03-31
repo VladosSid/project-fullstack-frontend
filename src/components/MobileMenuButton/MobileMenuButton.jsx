29bcc94f9f662fa36da7983eafd09a59a929222d
Victor Litvynchuk <vendettich13@icloud.com>
2023-03-31 16:47:35 +0300

    first commit

---
+++ src/components/MobileMenuButton/MobileMenuButton.jsx
@@ -0,0 +1,11 @@
+import menu from '../../images/menu.svg';
+
+const MobileMenuButton = () => {
+  return (
+    <button>
+      <img src={menu} alt="menu" />
+    </button>
+  );
+};
+
+export default MobileMenuButton;
