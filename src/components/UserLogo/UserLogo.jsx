786c74d1919c48dff23169c00e7ee87c153588b9
Victor Litvynchuk <vendettich13@icloud.com>
2023-04-01 19:31:20 +0300

    add some header styles

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,9 +1,12 @@
+import ava from '../../images/Header/ava.svg';
+import { UserLogoBtn } from 'components/UserLogo/UserLogo.styled';
+
 const UserLogo = () => {
   return (
-    <button>
-      <img src="" alt="" />
+    <UserLogoBtn>
+      <img src={ava} alt="avatar" />
       <p>Viktor</p>
-    </button>
+    </UserLogoBtn>
   );
 };
 
