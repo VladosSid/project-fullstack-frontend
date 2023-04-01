d15de102da19fce9b911678c651211bdc4175606
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-04-01 23:38:14 +0300

    feature/ShoppingList/css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,13 @@ import Header from './Header';
 import Footer from './Footer';
 
 import { AppBox } from './App.styled';
-
+import ShoppingListPage from '../pages/ShoppingListPage'
  
 export const App = props => {
   return (
     <AppBox>
       <Header />
-
+<ShoppingListPage/>
       <Footer />
     </AppBox>
   );
