9e70191dfdb4154a5694cdaae658c2ec11e38dd9
GannaKov <zlatta2000@gmail.com>
2023-04-01 17:30:41 +0200

    style input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,14 @@ import Header from './Header';
 import Footer from './Footer';
 
 import { AppBox } from './App.styled';
+import MainPage from 'pages/MainPage/MainPage';
 
- 
 export const App = props => {
   return (
     <AppBox>
       <Header />
-
+      <MainPage />
       <Footer />
     </AppBox>
   );
 };
-
