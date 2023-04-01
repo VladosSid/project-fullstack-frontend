90e44b8ac3928a94a5bdeaba996de0bb516c9108
GannaKov <zlatta2000@gmail.com>
2023-04-01 17:03:37 +0200

    app back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
-// import Header from './Header';
-// import Footer from './Footer';
+import Header from './Header';
+import Footer from './Footer';
 
-// import SearchPage from 'pages/SearchPage/SearchPage';
 import { AppBox } from './App.styled';
-import MainPage from 'pages/MainPage/MainPage';
 
 export const App = props => {
   return (
     <AppBox>
-      {/* <Header /> */}
-      <MainPage />
-      {/* <Footer /> */}
-      {/* <SearchPage /> */}
+      <Header />
+
+      <Footer />
     </AppBox>
   );
 };
