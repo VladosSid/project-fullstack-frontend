53c4b1e63d1817c8e8ae1deada43f138f595b1e7
GannaKov <zlatta2000@gmail.com>
2023-04-02 00:38:50 +0200

    window Width logika

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Recipes } from 'gannaFakeData';
+// import axios from 'axios';
 
 // import { Link } from 'react-router-dom';
 // import { Container, CardWrapper, RecipeTitle } from 'mainPage.styled';
@@ -14,6 +15,36 @@ import MainPageHero from 'components/MainPageHero';
 import DishCard from 'components/DishCard';
 
 export default function MainPage() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // const searchUrl = `${BASE_URL}search/movie?api_key=${keyApi}&query=${queryFilm}&page=`;
+  useEffect(() => {
+    let queryQuantity = 1;
+
+    if (width < 1441) {
+      queryQuantity = 2;
+    }
+    if (width < 769) {
+      queryQuantity = 1;
+    }
+    if (width >= 1441) {
+      queryQuantity = 4;
+    }
+    console.log('Здесь идет запрос на бек', width, 'query', queryQuantity); // Выводим данные, полученные с бекенда в консоль
+  }, [width]);
+
   const RecipesByCategory = Recipes.reduce((acc, recipe) => {
     if (!acc[recipe.category]) {
       acc[recipe.category] = [recipe];
