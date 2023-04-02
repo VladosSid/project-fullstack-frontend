730cdadf8565734c492d859fde2d31cdca5bf424
VladosSid <sidorsv121097@gmail.com>
2023-04-02 12:43:53 +0300

    fix App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
 import Header from './Header';
 import Footer from './Footer';
 
-import { AppBox, BoxBodyApp } from './App.styled';
+import { AppBox } from './App.styled';
 
 export const App = props => {
   return (
     <AppBox>
       <Header />
-
-      <BoxBodyApp>
-        {/* Тут всі компоненти сайту о крім Хедера та футера */}
-      </BoxBodyApp>
 
       <Footer />
     </AppBox>
