227de0aa238fbc303d243cd434353a569e4ef49d
Elena Solomko <lena06051981be@gmail.com>
2023-04-02 15:13:02 +0200

    update router

--- src/pages/TitlePageTemporary/TitlePageTemporary.jsx
+++ src/pages/TitlePageTemporary/TitlePageTemporary.jsx
@@ -1,7 +1,4 @@
-// import Footer from "components/Footer/Footer";
-// import Header from "components/Header/Header";
-import { Container } from "./MainPageTemporary.styled";
-// import { Container } from "./MainPageTemporary.styled";
+import { Container } from "./TitlePageTemporary.styled";
 
 const TemporaryAuthPage = () => {
     return (
@@ -10,8 +7,6 @@ const TemporaryAuthPage = () => {
             <p>This app offers more than just a collection of recipes -
                 it is designed to be your very own digital cookbook.
                 You can easily save and retrieve your own recipes at any time.</p>
-            {/* <Header />
-            <Footer /> */}
         </Container>
     )
 }
