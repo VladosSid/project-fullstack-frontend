34073c64fc34c8df27843289fc2250db4bab9942
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-03 12:27:14 +0300

    Welcome Page: basic structure

---
+++ src/components/WelcomePageHero/WelcomePageHero.jsx
@@ -0,0 +1,13 @@
+const WelcomePageHero = () => {
+    return (
+        // пока набрасываю только структуру, стили добавлю позже
+        <>
+            <h1>Welcome to the app</h1>
+            <p>This app offers more than just a collection of recipes -
+                it is designed to be your very own digital cookbook.
+                You can easily save and retrieve your own recipes at any time.</p>
+        </>
+    )
+}
+
+export default WelcomePageHero;
