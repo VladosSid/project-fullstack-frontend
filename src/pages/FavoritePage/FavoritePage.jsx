98775b5a82c2a2f731402bbc3f95cbe4d4498e18
VladosSid <sidorsv121097@gmail.com>
2023-04-04 19:30:38 +0300

    fix MyRecipes

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // eslint-disable-line
 import { recipesG } from 'gannaFakeData';
 import { useLocation } from 'react-router-dom';
-import instanceBacEnd from 'helpers/requestBackEnd';
-import { useNavigate } from 'react-router-dom';
+// import instanceBacEnd from 'helpers/requestBackEnd';
+// import { useNavigate } from 'react-router-dom';
 
 import { MainSection } from 'components/MainSection/MainSection';
 import FavoriteList from 'components/FavoriteList/FavoriteList';
@@ -11,7 +11,7 @@ import { Children } from 'react';
 
 const FavoritePage = () => {
   const location = useLocation();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <MainSection title={'Favorite'}>
