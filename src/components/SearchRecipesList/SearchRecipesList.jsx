986969b83b835be11cf3d34f370397c89f653e91
GannaKov <zlatta2000@gmail.com>
2023-04-05 10:03:53 +0200

    SearchRecipesList fetch

--- src/components/SearchRecipesList/SearchRecipesList.jsx
+++ src/components/SearchRecipesList/SearchRecipesList.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react'; //useState,
+import React, { useEffect, useState } from 'react'; //useState,
 // import { useLocation } from 'react-router-dom';
 // import axios from 'axios';
 // import RecipesList from './RecipesList';
-// import instanceBacEnd from 'helpers/requestBackEnd';
+import instanceBacEnd from 'helpers/requestBackEnd';
 export default function SearchRecipesList({ searchQuery, searchType }) {
   // const [recipes, setRecipes] = useState([]);
   // const location = useLocation();
   // const [searchValue, setSearchValue] = useState('');
   // const [searchType, setSearchType] = useState('query');
-  // const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     // const SQ = searchQuery.toLowerCase();
@@ -16,18 +16,18 @@ export default function SearchRecipesList({ searchQuery, searchType }) {
     // const SQ = searchQuery.query.toLowerCase();
     // console.log('SQ', SQ);
 
-    // instanceBacEnd.defaults.headers.common.Authorization =
-    //   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDI4NGEyNDI5ODkxOTJkMDJkOTc1ZGMiLCJpYXQiOjE2ODAzNjQzOTB9.F6KumBIsfhDh32UmksQgN3JqdFUpxwqq0ifHBL8dq3A';
-    // instanceBacEnd
-    //   .get(`/search?query=${searchQuery}&type=${searchType}`)
-    //   .then(function (response) {
-    //     setRecipes(response.data.result.data);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error.message);
-    //   });
+    instanceBacEnd.defaults.headers.common.Authorization =
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDI4NGEyNDI5ODkxOTJkMDJkOTc1ZGMiLCJpYXQiOjE2ODAzNjQzOTB9.F6KumBIsfhDh32UmksQgN3JqdFUpxwqq0ifHBL8dq3A';
+    instanceBacEnd
+      .get(`/search?query=${searchQuery}&type=${searchType}&limit=2`)
+      .then(function (response) {
+        setRecipes(response.data.result.data);
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
   }, [searchQuery, searchType]); //searchQuery
-
+  console.log('SP', recipes);
   return <div>Hier will be RecipesList</div>;
 }
 
