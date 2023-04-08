cd6f8046794b52922526ba89497a51711d5be76d
GannaKov <zlatta2000@gmail.com>
2023-04-08 17:23:18 +0200

    for something else tabl

--- src/components/SearchRecipesList/SearchRecipesList.styled.js
+++ src/components/SearchRecipesList/SearchRecipesList.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import search_mob from '../../images/mainPagePhoto/search_mob_opt.png';
+import search_tabl from '../../images/mainPagePhoto/search_tabl_opt.png';
 // import theme from '../../style/generalStyle';
 // export const GridContainer = styled.div`
 //   width: 100%;
@@ -33,29 +34,43 @@ export const GridContainer = styled.div`
 `;
 
 export const SRLNoItems = styled.div`
-  width: 228px;
-  height: 180px;
   background-repeat: no-repeat;
-  background-image: url('${search_mob}');
-  background-position: center top 10px;
-  background-size: 208px;
+
+  @media screen and (max-width: 767px) {
+    width: 228px;
+    height: 180px;
+    background-image: url('${search_mob}');
+    background-position: center top 10px;
+    background-size: 208px;
+  }
   @media screen and (min-width: 768px) {
+    width: 380px;
+    height: 291px;
+    background-image: url('${search_tabl}');
+    background-position: center top 15px;
+    background-size: 350px;
   }
 
   @media screen and (min-width: 1240px) {
   }
 `;
 export const SRLNoItemsText = styled.p`
-  margin-top: 170px;
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 500;
-  font-size: 14px;
+
   line-height: 1;
   letter-spacing: -0.02em;
   color: #000000;
   opacity: 0.5;
+  margin-left: 10px;
+  @media screen and (max-width: 767px) {
+    margin-top: 160px;
+    font-size: 14px;
+  }
   @media screen and (min-width: 768px) {
+    font-size: 24px;
+    margin-top: 266px;
   }
 
   @media screen and (min-width: 1240px) {
