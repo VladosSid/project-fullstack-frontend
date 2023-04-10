e930d611611f5a87eec032e7ac1b8566e0e25802
Olga Birkovska <aiba.aki9@gmail.com>
2023-04-10 20:17:03 +0300

    sign in form style adjustment

--- src/components/SigninForm/SigninForm.styled.js
+++ src/components/SigninForm/SigninForm.styled.js
@@ -43,6 +43,11 @@ export const FormTitle = styled.h2`
         font-size: 28px;
         line-height: 1.07;
     }
+
+    /* desktop */
+    @media screen and (min-width: 1025px) {
+        margin-bottom: 50px;
+    }
 `
 export const List = styled.ul`
     list-style: none;
