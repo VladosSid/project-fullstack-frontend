951cf9aff200e775db70915a5839e82b5124c13e
VladosSid <sidorsv121097@gmail.com>
2023-04-11 12:30:59 +0300

    fix

--- src/components/Recipe/RecipePreparation/RecipePreparation.styled.jsx
+++ src/components/Recipe/RecipePreparation/RecipePreparation.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-// import theme from '../../style/generalStyle';
+import theme from '../../style/generalStyle';
 
 export const PreparationWrapper = styled.div`
   margin-bottom: 100px;
