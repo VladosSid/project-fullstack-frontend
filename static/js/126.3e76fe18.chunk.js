526d233474e5060dd7521462c781302b677e8478
VladosSid <104505584+VladosSid@users.noreply.github.com>
2023-04-11 10:11:20 +0000

    Deploying to gh-pages from @ VladosSid/project-fullstack-frontend@3641939efbf82576a214fedfd3c4495caaa77109 🚀

---
+++ static/js/126.3e76fe18.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproject_fullstack_frontend=self.webpackChunkproject_fullstack_frontend||[]).push([[126],{9126:function(t,l,e){e.d(l,{lzl:function(){return r}});var n=e(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"}}]})(t)}}}]);
+//# sourceMappingURL=126.3e76fe18.chunk.js.map
